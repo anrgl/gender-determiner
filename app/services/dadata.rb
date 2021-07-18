b6fd0c407a10ea0dff5c096984c9feb91de7b16e
Aynur Galimov <galimovaynur@protonmail.com>
2021-07-18 19:35:20 +0500

    add dadata service

---
+++ app/services/dadata.rb
@@ -0,0 +1,22 @@
+class Dadata
+  def initialize(auth_token, x_secret, full_name)
+    @auth_token = auth_token
+    @x_secret = x_secret
+    @full_name = full_name
+  end
+
+  def response
+    headers = {:Authorization => @auth_token, :"X-Secret" => @x_secret, content_type: :json, accept: :json}
+    payload = [@full_name]
+    RestClient.post('https://cleaner.dadata.ru/api/v1/clean/name', payload.to_json, headers)
+  end
+
+  def gender(resp)
+    body = resp.body.force_encoding('UTF-8')
+    JSON.parse(body).first['gender']
+  end
+
+  def last_request(resp)
+    resp.headers[:date]
+  end
+end
