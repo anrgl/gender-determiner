08b1f809d8a943bab508ec1b78b258f71b1c1c20
Aynur Galimov <galimovaynur@protonmail.com>
2021-07-19 11:24:55 +0500

    is admin default

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_18_171202) do
+ActiveRecord::Schema.define(version: 2021_07_19_061832) do
 
   create_table "users", force: :cascade do |t|
     t.string "full_name"
     t.string "gender"
-    t.boolean "is_admin"
+    t.boolean "is_admin", default: false
     t.datetime "last_request"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
