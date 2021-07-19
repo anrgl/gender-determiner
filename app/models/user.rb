58eead5b1a37f08fca0b13023a7c887a157b44cb
Aynur Galimov <galimovaynur@protonmail.com>
2021-07-19 12:51:24 +0500

    update Dadata for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,12 +10,16 @@ class User < ApplicationRecord
     is_correct
   end
 
+  def admin?
+    is_admin
+  end
+
   private
   def last_request_and_gender
     auth_token = 'Token 7b11c5356f05ab5803e8ab3968c24b4dc4f2a9bc'
     x_secret = 'c69195188c7b13d12cbe4ac043be92bb28cb257d'
     full_name = self.full_name
-    resp = Dadata.new(auth_token, x_secret, full_name).response
+    resp = Dadata.new(auth_token, x_secret).response(full_name)
     self.gender = Dadata.gender(resp)
     self.last_request = Time.parse(Dadata.last_request(resp))
   end
