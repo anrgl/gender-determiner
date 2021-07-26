a82dc0fc6529ef5f5d58ef59e2bb7af6ce0003d4
Aynur Galimov <galimovaynur@protonmail.com>
2021-07-26 16:21:03 +0500

    fix: move dadata credentials

--- app/services/dadata.rb
+++ app/services/dadata.rb
@@ -1,7 +1,7 @@
 class Dadata < ApplicationService
   def self.check_gender(full_name)
-    auth_token = 'Token 7b11c5356f05ab5803e8ab3968c24b4dc4f2a9bc'
-    x_secret = 'c69195188c7b13d12cbe4ac043be92bb28cb257d'
+    auth_token = Rails.application.credentials.dadata[:auth_token]
+    x_secret = Rails.application.credentials.dadata[:x_secret]
     headers = {:Authorization => auth_token, :"X-Secret" => x_secret, content_type: :json, accept: :json}
     payload = [full_name]
     resp = RestClient.post('https://cleaner.dadata.ru/api/v1/clean/name', payload.to_json, headers)
